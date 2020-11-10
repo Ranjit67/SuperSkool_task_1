954725e8f1777446fd488d24c09afc91e68dc646
Ranjit67 <58407890+Ranjit67@users.noreply.github.com>
2020-11-10 17:21:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,68 +2,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app= express();
-// let data=[];
-// let k={
-//     prev:null,
-//     value:null,
-//     next:null
-// }
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.set("view engine","ejs");
 
-// app.get("/",(req,res)=>{
 
-// res.render("main", {date:data});
-// })
-
-// app.post("/",function(req,res){
-//     // let d = req.body.data;
-//     let head,tail;
-//     // data=[d,...data];
-//     head=tail=10;
-//     if(data.length === 0){
-       
-        
-//         k.value=req.body.data;
-//         k.prev=head;
-//         k.next=head;
-       
-//         data=[...data,k];
-//         console.log(data);
-//         k={
-//             prev:null,
-//             value:null,
-//             next:null
-//         }
-//     } else { if(data.length > 0){
-
-//         tail=9+data.length;
-       
-//         data[0].prev=tail+1;
-//         data[data.length-1].next=tail+1;
-    
-//         k.value=req.body.data;
-//         k.prev=tail;
-//         k.next=head;
-//        data.push(k);
-//        k={
-//         prev:null,
-//         value:null,
-//         next:null
-//     }
-//     } 
-//       console.log(data);
-        
-//     }
-
-// res.redirect("/")
-// });
 
 app.use(require("./router/duplicatevalue.js"));     //first task.
 app.use(require("./router/uniquevalue.js"));        //second task.
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT ||3000,()=>{
     console.log("The port is ready to start.");
 })
